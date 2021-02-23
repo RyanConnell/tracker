03c99097c63c52a8f0fb2e812c5d37c7d87cba37
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2021-02-23 21:34:49 +0000

    Add run function to binaries
    This allows to gracefully shutdown the application, while handing the
    error and keeping the error stack.

--- cmd/scraper/scraper.go
+++ cmd/scraper/scraper.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"tracker/trackable/show"
 )
 
 func main() {
-	fmt.Printf("Starting...\n")
-	err := show.ScrapeAll()
-	if err != nil {
-		fmt.Printf("Error encountered; %v", err)
+	if err := run(); err != nil {
+		log.Printf("%+v\n", err)
+		os.Exit(1)
 	}
+}
+
+func run() error {
+	log.Printf("starting scraper")
+	if err := show.ScrapeAll(); err != nil {
+		return fmt.Errorf("scrape error: %w", err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module tracker
 go 1.15
 
 require (
-	cloud.google.com/go v0.27.0 // indirect
+	cloud.google.com/go v0.78.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.0
 	github.com/gorilla/mux v1.6.2
 	github.com/gorilla/sessions v1.1.2
-	golang.org/x/net v0.0.0-20180911220305-26e67e76b6c3
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
+
 )
